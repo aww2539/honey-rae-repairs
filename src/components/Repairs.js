8e8a9c2e675724e5985525b9d3bb875ecd67c282
Alex Woodall <aww2539@gmail.com>
2021-08-26 14:25:06 -0500

    created customer list

---
+++ src/components/Repairs.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { CustomerList } from "./customers/CustomerList"
+
+
+
+export const Repairs = () => {
+    return (
+        <>
+            <h1>Honey Rae's Repair Shop</h1>
+            <CustomerList />
+        </>
+    )
+}
